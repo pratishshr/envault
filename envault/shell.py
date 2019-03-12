4052e8e0f987c17eeae1187ada5729c86b3f049f
pratishshr <pratishshr@gmail.com>
2019-03-12 13:17:38 +0545

    Update os environments

--- envault/shell.py
+++ envault/shell.py
@@ -4,5 +4,5 @@ import subprocess
 
 def run_with_env(command, env):
     """ Run command with injected environment variables """
-    environment_variables = {**env}
+    environment_variables = {**os.environ, **env}
     subprocess.Popen(command, shell=True, env=environment_variables)
