79d2ffd9aec47220dad0a871514c4910d31d0e37
pratishshr <pratishshr@gmail.com>
2019-10-03 19:30:41 +0545

    Update version

--- envault.go
+++ envault.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	info := &cli.Info{
 		Name:        "Envault",
-		Version:     "1.1.2",
+		Version:     "1.1.3",
 		Description: "Envault is a simple CLI tool to run a process with secrets from AWS Secrets Manager.",
 		AuthorName:  "Pratish Shrestha",
 		AuthorEmail: "pratishshr@gmail.com",
